644ed7fe4414a8ccee1710b8d02e46efaf68f262
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-02-28 12:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const container = document.getElementById('infi-list');
+container.addEventListener('scroll',(e)=>{
+	const remainingScroll = container.scrollHeight-container.clientHeight;
+	const scrolledRemaining = remainingScroll-container.scrollTop;
+// console.log(container.scrollHeight-container.clientHeight-container.scrollTop);
+	if(scrolledRemaining<=2){
+		itemGenerator(2);
+	}
+});
+let itemCount = 1;
+function itemGenerator(n){
+	for(let i=1;i<=n;i++){
+		const newItem = document.createElement('li');
+		newItem.innerText = `Item ${itemCount++}`;
+		container.append(newItem);
+	}
+}
 
+itemGenerator(10);
